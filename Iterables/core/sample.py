38e0e431178272386a1744a5bd4f73ef20d03298
NiranjanNaiduSetha <niranjan1223@gmail.com>
2023-01-06 17:21:16 +0530

    Sample code for iterables

--- Iterables/core/sample.py
+++ Iterables/core/sample.py
@@ -1,9 +1,8 @@
-from itertools import *
-n = input()
-a = list(input().split(" "))
-t = int(input())
-c = 0
-for i in combinations(a,t):
-    if "a" in i:
-        c+=1
-print(c/len(list(combinations(a,t))))
+import math
+
+def iterate_module(size,input,K):
+    Count = len(list(filter(lambda x: x == "a",input))) #lambda expression
+    total = math.comb(size,K) #combinations
+    withoutA = math.comb(size-Count, K)
+    result = 1 - withoutA/total
+    return "{0:.3f}".format(result) # decimal point value upto 3 decimals
