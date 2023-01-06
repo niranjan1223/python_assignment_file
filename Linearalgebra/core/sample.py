9e2c2ea91919ef9fb1a045ed8aad67ad599e6946
NiranjanNaiduSetha <niranjan1223@gmail.com>
2023-01-06 18:15:15 +0530

    sample code for linear_algebra

--- Linearalgebra/core/sample.py
+++ Linearalgebra/core/sample.py
@@ -1,9 +1,6 @@
-import numpy
-n, m = input().split()
-lis = []
-for i in range(int(n)):
-    lis.append(list(map(int, input().split())))
-my_array = numpy.array(lis)
-print(numpy.mean(my_array, axis = 1))
-print(numpy.var(my_array,axis = 0))
-print(round((numpy.std(my_array)), 11))
+#numpy contains linearalgebra (linalg) as sub_module
+#linearalgebra.det to perform determinant function
+
+import numpy as np
+def linear_algebra(arr):
+    return np.linalg.det(arr).round(2)
