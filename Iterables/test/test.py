01fb2342e10e195049d6f5c7139345f084078b01
NiranjanNaiduSetha <niranjan1223@gmail.com>
2023-01-06 17:21:33 +0530

    Sample code for iterables

---
+++ Iterables/test/test.py
@@ -0,0 +1,13 @@
+import Iterables.core.sample as i
+import unittest
+
+class testcase(unittest.TestCase):
+    def test_Iterate(self):
+        size=4
+        input=['a','a','c','d']
+        k=2
+        result= i.iterate_module(size,input,k)
+        self.assertEqual(result,'0.833')
+
+if __name__ == '__main__':
+    unittest.main()
