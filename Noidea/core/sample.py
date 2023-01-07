798ee5ae01c930bda4a30e906e74113c3d81e096
NiranjanNaiduSetha <niranjan1223@gmail.com>
2023-01-07 13:36:14 +0530

    Sample code for disjoint sets

--- Noidea/core/sample.py
+++ Noidea/core/sample.py
@@ -1,9 +1,11 @@
-n, m = input().split()
-arr = list(input().split())
-setA = set(input().split())
-setB = set(input().split())
-
-arrA = [ i for i in arr if i in setA]
-arrB = [ i for i in arr if i in setB]
-
-print(len(arrA) - len(arrB))
+# disjoint sets
+def disjoint(s,A,B): #s is list
+    a=set(A)
+    b=set(B)
+    happiness=0
+    for i in s:
+        if i in A:
+            happiness += 1
+        if i in B:
+            happiness -= 1
+    return happiness
