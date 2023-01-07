1049d7f2b5daeab75f20453ddb6ddb4c46f4cf83
NiranjanNaiduSetha <niranjan1223@gmail.com>
2023-01-07 12:09:51 +0530

    sample code for Merge Tools # remove duplicates in substrings

--- Mergetools/core/sample.py
+++ Mergetools/core/sample.py
@@ -1,3 +1,5 @@
+
+#remove duplicates in sub strings
 def merge_the_tools(string, k):
     if k == 1:
         for n in range(len(string)):
@@ -6,3 +8,5 @@ def merge_the_tools(string, k):
         for n in range(k):
             print("".join(sorted(set(string[:k]), key=string.index)))
             string = string[k:]
+
+
